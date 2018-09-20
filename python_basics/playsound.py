60d31c992bdfc32ce58170d07814e64314221aa6
Yannick Adriaansen <yannick.adriaansen@student.hku.nl>
2018-09-20 23:01:43 +0200

    initial commit : modified kick.wav, modified playsound.py

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,10 +1,31 @@
+import time
+import os
+
+
 print("hello, how many KICKS would you like?")
-x = int(input())
-for i in range(x):
-    import os
+numPbtimes = int(input())
+
+print("BPM?")
+bpm = float(60/float(input()))
+print(bpm)
+
+print("ritme??:")
+lijst = [float(x) for x in range(numPbtimes)]
+ritme = []
+length = len(lijst)
+
+for f in range(length):
+    data=float(input())
+    ritme.append(data)
+
+for value in ritme:
+    tsleep = float(float(value) *float(bpm))
     os.system("aplay /media/yannick/NieuwVolume/HKU/CSD_Rep/CSD2/python_basics/kick.wav")
-    import time
-    time.sleep(0.5)
+    time.sleep(tsleep)
+    
+    
+
+
 
 
 
