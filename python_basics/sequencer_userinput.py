e4b6c13ec60d864f03d792b8cf900ab6446d0b98
Yannick Adriaansen <yannick.adriaansen@student.hku.nl>
2018-10-05 21:11:23 +0200

    fixed : option for wrong user input "change bpm?"

--- python_basics/sequencer_userinput.py
+++ python_basics/sequencer_userinput.py
@@ -1,6 +1,7 @@
 #variables
 import simpleaudio as sa
 import time
+import random
 samples = [sa.WaveObject.from_wave_file("/media/yannick/NieuwVolume/HKU/CSD_Rep/CSD2/python_basics/audio_files/kick.wav")]
 
 
@@ -15,28 +16,40 @@ c = input()
 if c=="n":
   print("Set bpm to?") 
   bpm = float(input())
-if not c=="n" or c=="y":
+if not ((c=="y") or (c=="y")):
   bpm = 60
   print("you fool! you pressed the wrong button. your bpm is now 60.")   
 
 #variables2
+print(bpm)
 quarterNoteDuration = 60 / bpm
+print(quarterNoteDuration)
 sixteenthNoteDuration = quarterNoteDuration / 4.0
 
 #create list for timestamps
 timestamps = []
+timestamps16th = []
 
 #ask for rhytm, input[list] 
-print("rhytm?") 
+template = """input rhytm:\n\
+notelegths:\n \
+0.25 = 16th note\n \
+0.5 = 8th note\n \
+1 = quarternote\n \
+etc."""
+print(template)
+
+
+#for every input in rhytm: 
+#add element(float) to list, convert to the right note durations, 
+#add to previous value to get timing of the 16th note, based on where the measure starts (0): 
 rhytm = [0]
 duration = [float(x) for x in input().split()]
-
-#voor elke input van ritme, voeg: 
 for i in range (len(duration)):
   rhytm.append(float(duration[i]) * 4 + float(rhytm[i]))
 print(rhytm)
 
-# create a list withtimestamps
+# create a list with timestamps
 timestamps = []
 for timestamp in rhytm:
   timestamps.append(timestamp * sixteenthNoteDuration)
